93d880b5f7d99da8849f55df9e12126c39a5e57d
Muhammadsiddiq-code <xamidullayevich10@gmail.com>
2026-01-26 11:06:32 +0500

    added all function

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/auth.controller");
 // Userlar uchun (Telegram)
 router.post("/login", authController.login);
 
-// Adminlar uchun (Yangi)
+// Adminlar uchun (Web Panel) - MANA SHU QATOR BO'LISHI KERAK
 router.post("/admin-login", authController.adminLogin);
 
 module.exports = router;
